67e308e0fa9ed17f64209da6bffca5a70702a916
KenSauro27 <167883474+KenSauro27@users.noreply.github.com>
2025-09-20 07:42:44 -0700

    Add files via upload

---
+++ client/includes/navbar.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg navbar-dark p-4" style="background-color: #023E8A;">
+  <a class="navbar-brand" href="index.php">Client Panel</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Categories
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <?php
+          $categoriesWithSubcategories = $subcategoryObj->getCategoriesWithSubcategories();
+          foreach ($categoriesWithSubcategories as $category) {
+            echo '<h6 class="dropdown-header">' . $category['category_name'] . '</h6>';
+            foreach ($category['subcategories'] as $subcategory) {
+              echo '<a class="dropdown-item" href="#">' . $subcategory['subcategory_name'] . '</a>';
+            }
+          }
+          ?>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="project_offers_submitted.php">Project Offers Submitted </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="profile.php">Profile</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="core/handleForms.php?logoutUserBtn=1">Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
